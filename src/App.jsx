ccc3e2ee78cea1bda8754053292d6c6bb41b55ac
ymahdi2023F <ymahdi@vt.edu>
2023-11-19 22:15:01 -0500

    updated search bar feature. (need to debug)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './components/Logo';
 import BackgroundImage from './components/BackgroundImage';
+import SearchBar from './components/searchBar';
 import './styles.css'; 
 import './App.css';
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const handleSearch = async (searchTerm) => {
+    const apiKey = '1992880a4cc3a11d89181567ff857552';
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      setSearchResults(data.results);
+    } catch(error) {
+      console.error('Error fetching data:', error);
+    }
+  }
   return (
     <div className="App">
       <nav className="App-nav">
@@ -12,6 +26,15 @@ function App() {
       </nav>
       <header className='App-header'>
         <BackgroundImage />
+        <SearchBar onSearch={handleSearch} />
+        <div className='movie-grid'>
+          {searchResults.map((movie) => (
+            <div key = {movie.id} className='movie-card'>
+              <img src= {`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} />
+              <p>{movie.title}</p>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
